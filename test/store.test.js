396bbe9eed154d619c51cefdcd9f53ec2f1cd7be
Joachim Kainz <jfk@jolira.com>
2012-08-14 22:26:23 -0700

    add standalone unit testing.

--- test/store.test.js
+++ test/store.test.js
@@ -13,53 +13,96 @@
      *   "aws-region": "us-west-1"
      * }
      */
-    var OBJ = {a:1, b:2, c:3, d:4 },
+    var MockS3 = function () {
+            var putCount = 0,
+                getCount = 0;
+
+            this.PutObject = function (arg, cb) {
+                switch (putCount++) {
+                    case 0:
+                        return cb(undefined, {
+                            Headers:{
+                                etag:"\"c471a30c5517c593dbc2dc310f6bfbc5\""
+                            }
+                        });
+                    case 1:
+                        return cb(undefined, {
+                            Headers:{
+                                etag:"\"3c1eb0c39a16516e57b57b3394c2f7f5\""
+                            }
+                        });
+                }
+            };
+            this.GetObject = function (arg, cb) {
+                switch (getCount++) {
+                    case 0:
+                        return cb(undefined, {
+                            Headers:{
+                                etag:"c471a30c5517c593dbc2dc310f6bfbc5"
+                            },
+                            Body:"{\"vals\":{\"a\":1,\"b\":2,\"c\":3,\"d\":4},\"mods\":{}}"
+                        });
+                    case 1:
+                        return cb(undefined, {
+                            Headers:{
+                                etag:"\"3c1eb0c39a16516e57b57b3394c2f7f5\""
+                            },
+                            Body:"{\"vals\":{\"a\":111,\"c\":3,\"d\":4},\"mods\":{\"a\":1345007357931,\"b\":1345007357931}}"
+                        });
+                }
+            };
+            this.DeleteObject = function (arg, cb) {
+                return cb();
+            }
+        },
+        OBJ = {a:1, b:2, c:3, d:4 },
         URL = "test/1",
         now = Date.now(),
         loader = require('server-config'),
         assert = require('assert'),
         vows = require('vows'),
+        horaa = require('horaa'),
         store = require('../lib/store'),
         removeTest = {
-            topic: function (db, data, etag) {
+            topic:function (db, data, etag) {
                 db.remove(URL, this.callback);
             },
-            "make sure there is no error": function(err) {
+            "make sure there is no error":function (err) {
                 assert.isUndefined(err);
             }
         },
         readTest = {
-            topic: function (db, data, etag) {
+            topic:function (db, data, etag) {
                 var self = this;
 
-                db.read(URL, function(err, data, etag) {
+                db.read(URL, function (err, data, etag) {
                     self.callback(err, db, data, etag);
                 });
             },
-            "make sure there is no error and we got data back": function(err, db, data, etag) {
+            "make sure there is no error and we got data back":function (err, db, data, etag) {
                 assert.isNull(err);
                 assert.isString(etag);
-                assert.deepEqual({ a: 111, d: 4, c:3 }, data);
+                assert.deepEqual({ a:111, d:4, c:3 }, data);
             },
-            "delete": removeTest
+            "delete":removeTest
         },
         updateTest = {
-            topic: function (db, data, etag) {
+            topic:function (db, data, etag) {
                 var self = this;
 
                 db.update(URL, Date.now(), {
-                    a: 111,
-                    b: null
-                }, function(err, data, etag) {
+                    a:111,
+                    b:null
+                }, function (err, data, etag) {
                     self.callback(err, db, data, etag);
                 });
             },
-            "make sure there is no error and we got data back": function(err, db, data, etag) {
+            "make sure there is no error and we got data back":function (err, db, data, etag) {
                 assert.isNull(err);
                 assert.isString(etag);
-                assert.deepEqual({ a: 111, d: 4, c:3 }, data);
+                assert.deepEqual({ a:111, d:4, c:3 }, data);
             },
-            "read": readTest
+            "read":readTest
         },
         createTest = {
             "topic":function (db) {
@@ -69,7 +112,7 @@
                     self.callback(err, db, data, etag);
                 });
             },
-            "make sure there is no error and we got data back": function(err, db, data, etag) {
+            "make sure there is no error and we got data back":function (err, db, data, etag) {
                 assert.isNull(err);
                 assert.isString(etag);
                 assert.deepEqual(OBJ, data);
@@ -78,8 +121,20 @@
         },
         openTest = {
             "topic":function (config) {
+                if (config["aws-access-key-id"] === "aws-access-key-id" || config["aws-secret-access-key"] === "aws-secret-access-key") {
+                    var awssum = horaa('awssum');
+
+                    awssum.hijack('load', function (arg) {
+                        assert.equal(arg, 'amazon/s3');
+
+                        return {
+                            S3:MockS3
+                        };
+                    });
+                }
+
                 return store({
-                    info: function() {
+                    info:function () {
                         console.log.apply(console.log, arguments);
                     }
                 }, config, this.callback);
@@ -92,11 +147,11 @@
         'load the configuration':{
             topic:function () {
                 loader({
-                    "aws-account-id":undefined,
-                    "aws-access-key-id":undefined,
-                    "aws-secret-access-key":undefined,
-                    "aws-region":undefined,
-                    "aws-bucket":undefined,
+                    "aws-region":"aws-region",
+                    "aws-account-id":"aws-account-id",
+                    "aws-access-key-id":"aws-access-key-id",
+                    "aws-secret-access-key":"aws-secret-access-key",
+                    "aws-store-bucket":"aws-store-bucket",
                     "application-name":"test"
                 }, "~/.site-manager.json", this.callback);
             },
